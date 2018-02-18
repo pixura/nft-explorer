6e3c20d463aab312b2da6f073c822ee359ed56ea
John Crain <john@pixura.io>
2018-02-17 22:39:03 -0700

    Collectibles endpoint

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.set("port", process.env.PORT || 3031);
 
+const API_URL = "http://192.168.43.108:5000/graphql";
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -19,6 +21,53 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+// get all collectibles
+app.get('/api/collectibles', (req, res) => {
+  const address = req.params.address;
+  const gqlQuery = {
+    query:`{
+      allCollectibles(orderBy:PRIMARY_KEY_ASC) {
+        nodes{
+          collectibleOwner
+          collectibleTokenId
+          collectibleInstagramId
+          collectibleLastSoldPrice
+          collectibleCurrentBidder
+          collectibleCurrentBidPrice
+          collectibleCreator
+        }
+      }
+      allImageIdToImageUrls {
+        nodes {
+          instaId
+          url
+        }
+      }
+    }`
+  };
+
+  axios.post(API_URL, gqlQuery)
+  .then(response => {
+    let collectibles =  response.data.data.allCollectibles.nodes;
+    const imageData = response.data.data.allImageIdToImageUrls.nodes;
+    let myCollectibles = [];
+    collectibles.map((collectible, i) => {
+      const image = imageData.filter( obj => {
+        return obj.instaId === collectible.collectibleInstagramId;
+      });
+      collectible.imgUrl = image[0].url;
+    })
+    res.send(JSON.stringify({status:'SUCCESS', result:response.data }));
+  })
+  .catch(error => {
+    console.log(error)
+    res.send(JSON.stringify({status:'FAIL', result:error }));
+  });
+})
+
+// get all transfers
+// get all bids
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
