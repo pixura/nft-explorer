c71802d75edf65f84487fddf0b597319474d9f6c
Jonathan Perkins <jroyal55@gmail.com>
2018-02-17 23:06:19 -0700

    add data contract

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import './index.css';
 
+// {
+//   collectibleOwner: '8cc8da168762e1c8f27cb36be90d23e075b391ee',
+//   collectibleTokenId: '0000000000000000000000000000000000000000000000000000000000000001',
+//   collectibleInstagramId: '1346330791181662504_51228965',
+//   collectibleLastSoldPrice: null,
+//   collectibleCurrentBidder: null,
+//   collectibleCurrentBidPrice: null,
+//   collectibleCreator: '8cc8da168762e1c8f27cb36be90d23e075b391ee',
+//   imgUrl: 'https://...'
+// }
+
+
 class Dashboard extends Component {
   render() {
     return (
