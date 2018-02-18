241c62b7de767215e12b6f8dbb3c7e14cdb98b2e
John Crain <john@pixura.io>
2018-02-18 01:46:02 -0700

    Review title change

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Pixura, motherfuckers</h1>
+          <h1 className="App-title">Non-fungible token explorer</h1>
           <Dashboard collectibles={ this.state.collectibles }></Dashboard>
         </header>
       </div>
