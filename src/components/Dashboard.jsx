23352fab43d3af4bc61bb34498827348392e484b
Jonathan Perkins <jroyal55@gmail.com>
2018-02-18 01:39:00 -0700

    format data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -27,11 +27,17 @@ const Dashboard = (props) => {
     console.log('collectible', c)
     return (
       <div className="coll-container" key={c.collectibleInstagramId}>
-        <img className="coll-img" src={ c.imgUrl } alt="collectible"/>
+        <p className="coll-id">Asset ID:{c.collectibleTokenId}</p>
         <p className="coll-owner" >Owner:
           <a href={`https://ropsten.etherscan.io/address/${c.collectibleOwner}`} target="_blank">{c.collectibleOwner}</a>
         </p>
-        <p className="coll-id">Asset ID:{c.collectibleTokenId}</p>
+        <p className="coll-creator">
+          Creator:
+          <a href={`https://ropsten.etherscan.io/address/${c.collectibleCreator}`} target="_blank">{c.collectibleCreator}</a>
+        </p>
+        <div className="coll-img-container">
+          <img className="coll-img" src={ c.imgUrl } alt="collectible"/>
+        </div>
       </div>
     )
   })
